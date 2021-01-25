7fb66ab45d348df37cb71702306b1cc883d4f5ca
frayfray248 <nicman248@hotmail.com>
2021-01-25 13:23:44 -0800

    About

---
+++ src/styledComponents/StyledContainer.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import Container from 'react-bootstrap/Container';
+
+const BootStrapContainer = ({...props}) => (<Container {...props}/>)
+
+const StyledContainer = styled(Container)`
+    color: white;
+    border: 2px solid white;
+    border-radius: 10px;
+    padding: 10px;
+`
+
+export default StyledContainer;
