307e9fb8d9155cfd5d1431a751344eb7f53e793e
frayfray248 <nicman248@hotmail.com>
2021-01-25 15:45:27 -0800

    StyledButton shadow

--- src/styledComponents/StyledButton.js
+++ src/styledComponents/StyledButton.js
@@ -7,7 +7,7 @@ const StyledButton = styled(BootstrapButton)`
     background: black;
     border: none;
     border-radius: 10px;
-
+    box-shadow: 5px 5px 4px black;
     &:hover {
         background: purple;
     }
