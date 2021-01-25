469a6d45d0567ba88e8875d8163da1751b77ec48
frayfray248 <nicman248@hotmail.com>
2021-01-25 15:31:53 -0800

    PortfolioItem

---
+++ src/components/PortfolioItem.js
@@ -0,0 +1,26 @@
+import Hr from '../styledComponents/StyledHr';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from '../styledComponents/StyledProfilePicture';
+import Button from '../styledComponents/StyledButton';
+
+const PortfolioItem = ({ title, profilePic, description, projectURL, links }) => (
+    <div>
+        <Row>
+            <Col>
+                <h1>{title}</h1>
+                <Hr bgColor={'black'} />
+                <Image src={profilePic} float />
+                <p>{description}</p>
+                <br />
+                <Button href={projectURL}>Project</Button>
+                {links? (links.map((link, index) => (
+                    <Button id={index} href={link.url}>{link.name}</Button>
+                ))) : '' }
+            </Col>
+        </Row>
+        <Hr bgColor={'black'} />
+    </div>
+)
+
+export default PortfolioItem;
