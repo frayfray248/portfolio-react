ea4896013327c4c8a6f2742c37d16a45a5c728d9
frayfray248 <nicman248@hotmail.com>
2021-01-25 15:30:18 -0800

    portfolio route

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PortfolioNavbar from './styledComponents/StyledPortfolioNavbar';
 import Footer from './styledComponents/StyledFooter';
 import Landing from './styledComponents/StyledLanding';
 import About from './components/About';
+import Portfolio from './components/Portfolio';
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
         <Switch>
           <Route exact path="/"> <Landing /> </Route>
           <Route path="/about"> <About /></Route>
+          <Route path="/portfolio"> <Portfolio /></Route>
           <Route><h1 class='text-center' style={{color: 'white'}}>Oops! Page not found!</h1></Route>
         </Switch>
 
