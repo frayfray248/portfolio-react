8d4428d48b607f8054bdc7f0540f2fd11d2cb21f
frayfray248 <nicman248@hotmail.com>
2021-01-25 15:37:45 -0800

    footer icon hrefs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,16 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Icon from '../styledComponents/StyledIcon';
 
-const Footer = ({className}) => (
+const Footer = ({ className }) => (
     <div className={`${className} p-3 text-center`}>
         <Row>
             <Col className={'mt-1'}> Fraser Macallum </Col>
-            <Col> <Icon href='#' className='fa fa-linkedin'/> <Icon className='fa fa-github'/></Col>
+            <Col>
+                <Icon href='https://www.linkedin.com/in/fraser-macallum-752b2716b' className='fa fa-linkedin' />
+                <Icon href='https://github.com/frayfray248' className='fa fa-github' />
+            </Col>
         </Row>
-        
+
     </div>
 )
 
